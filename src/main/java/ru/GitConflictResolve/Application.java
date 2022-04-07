48060bda4996fd4a0310a897b6a316338e35f1f4
vsemakin <Valeriy.Semakin@vsegda-da.com>
2022-04-07 09:53:50 +0400

    Commit origin

--- src/main/java/ru/GitConflictResolve/Application.java
+++ src/main/java/ru/GitConflictResolve/Application.java
@@ -2,6 +2,8 @@ package ru.GitConflictResolve;
 
 public class Application {
     public static void main(String[] args){
+
         System.out.println("Simple text");
+        System.out.println("Now commit origin branch");
     }
 }
